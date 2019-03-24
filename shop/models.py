411b6cf440665a58415fa6dcf65dca967f10fa56
kimplausible <andyk1278@gmail.com>
2019-03-23 19:22:44 -0700

    Catalog views for product_list and product_detail created.

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -12,6 +13,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                        args=[self.slug])
 
 class Product(models.Model):
     # many-to-one relationship: product belongs to one category - category contains many products
@@ -38,3 +43,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
